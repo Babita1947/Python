9d5c8973b90de3d15fda07daad3921c1c3892af7
Babita1947 <130829271+Babita1947@users.noreply.github.com>
2024-01-15 16:24:23 +0530

    added update the file

--- Lecture_11/Q_slicing_operator.py
+++ Lecture_11/Q_slicing_operator.py
@@ -1,7 +1,14 @@
 # Write a python script to reverse a given string word-wise.
 
-s = "Mysirg education services private limited"
-print(' '.join(s.split(' ')[::-1]))
+# s = "Mysirg education services private limited"
+# print(' '.join(s.split(' ')[::-1]))
 
-# in single line
-print(' '.join(input("Enter a string : ").split(' ')[: :-1]))
+# # in single line
+# print(' '.join(input("Enter a string : ").split(' ')[: :-1]))
+
+
+
+
+
+s = "mysirg education services private limited"
+print(' '.join(s.split(" ")[::-1]))
